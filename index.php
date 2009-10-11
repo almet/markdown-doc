db1470ecce261242a5f271632ec6495615e6055e
Alexis Metaireau <ametaireau@gmail.com>
2009-10-11 17:07:48 +0200

    switching default theme to spiral

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once 'libs/markdown.php';
 
 $article = 'articles/'.$_GET['article'].'.md';
-$theme = (isset($_GET['theme'])) ? $_GET['theme'] : 'notmyidea';
+$theme = (isset($_GET['theme'])) ? $_GET['theme'] : 'spiral';
 
 if(file_exists($article)){
 	$content = file_get_contents($article);

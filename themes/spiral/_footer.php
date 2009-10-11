d1a772749987341df4912b0181b96c1e024c228f
Alexis Metaireau <ametaireau@gmail.com>
2009-10-11 17:11:44 +0200

    add spiral theme

---
+++ themes/spiral/_footer.php
@@ -0,0 +1,12 @@
+
+		</div>
+	</div>
+	
+	<div id="footer">
+		Spiral is free software under GNU/GPL license. Thanks for your contributions.
+	</div>
+
+</div>
+
+</body>
+</html>
---
+++ themes/spiral/_header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Spiral</title>
+	<link rel="stylesheet" href="themes/spiral/styles/main.css" type="text/css" media="screen" charset="utf-8" />
+	<link rel="icon" href="themes/spiral/images/favicon.png" type="image/png" />
+</head>
+<body>
+
+<div id="layout">
+	
+	<div id="header">
+		<h1 id="main_title"><span>Spiral</span></h1>
+
+		<div id="main_menu">
+			<ul>
+				<li class="home">
+					<a href=""><span>Home</span>
+					<span class="description"></span></a></li>
+				<li class="about">
+					<a href=""><span>About</span>
+					<span class="description"></span></a></li>
+				<li class="downloads">
+					<a href=""><span>Downloads</span>
+					<span class="description"></span></a></li>
+				<li class="documentation">
+					<a href="" class="selected"><span>Documentation</span>
+					<span class="description"></span></a></li>
+				<li class="planet">
+					<a href=""><span>Planet</span>
+					<span class="description"></span></a></li>
+				<li class="community">
+					<a href=""><span>Community</span>
+					<span class="description"></span></a></li>
+			</ul>
+			<div class="clear"></div>
+		</div>
+	</div>
+	
+	<div id="body">
+		<div id="content">
